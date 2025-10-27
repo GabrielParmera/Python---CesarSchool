303edd88c62d720dc80fce28d442f3f227b5a9c8
GabrielParmera <grp@cesar.school>
2025-10-27 17:12:44 -0300

    incluir def

--- Fichas de exercícios/Ficha 03/qst04.py
+++ Fichas de exercícios/Ficha 03/qst04.py
@@ -1,9 +1,17 @@
-N = int(input())
-votes = list(map(int, input().split()))
+def verificar_impeachment(total_jogadores, votos):
+  votos_favoraveis = 0
+  for voto in votos:
+    if voto == 1:
+      votos_favoraveis += 1
 
-impeachment_votes = sum(votes)
+  if votos_favoraveis >= (2/3) * total_jogadores:
+    return "impeachment"
+  else:
+    return "acusacao arquivada"
 
-if impeachment_votes >= (2/3) * N:
-    print("impeachment")
-else:
-    print("acusacao arquivada")
+total_jogadores = int(input())
+votos_str = input()
+votos = list(map(int, votos_str.split()))
+
+resultado = verificar_impeachment(total_jogadores, votos)
+print(f"{resultado}")
